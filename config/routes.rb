02c8d11a9f200509924fd3cb1157ae9dfa4503d5
Joakim Runeberg <joakim.runeberg@gmail.com>
2015-08-18 09:28:56 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
-Spree::Core::Engine.add_routes do
+Spree::Core::Engine.routes.prepend do
   # Add your extension routes here
   resources :orders do
     resource :checkout, :controller => 'checkout' do
       member do
-        get :paytrail_cancel
-        get :paytrail_return
         get :skrill_cancel
         get :skrill_return
+        get :paytrail_cancel
+        get :paytrail_return
       end
     end
   end
 
-  post '/paytrail' => 'paytrail_status#update'
-  post '/skrill' => 'skrill_status#update'
+  match '/paytrail' => 'paytrail_status#update', :via => :get, :as => :paytrail_status_update
+  match '/skrill' => 'skrill_status#update', :via => :post, :as => :skrill_status_update
 end
-
-
-  
